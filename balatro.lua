66b5a008d32382bb7dd5d621b83d429983841185
Kitty-knight <sp2000@outlook.com>
2024-11-12 15:38:33 +0000

    Clubs & Diamonds
    Added options for Diamonds and Clubs

--- balatro.lua
+++ balatro.lua
@@ -35,6 +35,39 @@ SMODS.Atlas{
             atlas_table = "ASSET_ATLAS"
         }
 
+SMODS.Atlas{
+            key = "balatro_diamonds_1",
+            path = "balatro_diamonds_1.png",
+            px = 71,
+            py = 95,
+            atlas_table = "ASSET_ATLAS"
+        }
+
+SMODS.Atlas{
+            key = "balatro_diamonds_2",
+            path = "balatro_diamonds_2.png",
+            px = 71,
+            py = 95,
+            atlas_table = "ASSET_ATLAS"
+        }
+
+SMODS.Atlas{
+            key = "balatro_clubs_1",
+            path = "balatro_clubs_1.png",
+            px = 71,
+            py = 95,
+            atlas_table = "ASSET_ATLAS"
+        }
+
+SMODS.Atlas{
+            key = "balatro_clubs_2",
+            path = "balatro_clubs_2.png",
+            px = 71,
+            py = 95,
+            atlas_table = "ASSET_ATLAS"
+        }
+
+
 SMODS.DeckSkin{
             key = "balatro_hearts",
             suit = "Hearts",
@@ -55,6 +88,30 @@ SMODS.DeckSkin{
             hc_atlas = "balatro_spades_2",
             loc_txt = {
 				["en-us"] = "Balatro Spades"
+			},
+            posStyle = "collab"
+        }
+
+SMODS.DeckSkin{
+            key = "balatro_diamonds",
+            suit = "Diamonds",
+            ranks =  {"Jack", "Queen", "King"},
+            lc_atlas = "balatro_diamonds_1",
+            hc_atlas = "balatro_diamonds_2",
+            loc_txt = {
+				["en-us"] = "Balatro Diamonds"
+			},
+            posStyle = "collab"
+        }
+
+SMODS.DeckSkin{
+            key = "balatro_clubs",
+            suit = "Clubs",
+            ranks =  {"Jack", "Queen", "King"},
+            lc_atlas = "balatro_clubs_1",
+            hc_atlas = "balatro_clubs_2",
+            loc_txt = {
+				["en-us"] = "Balatro Clubs"
 			},
             posStyle = "collab"
         }
